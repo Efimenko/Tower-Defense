96132ee2cc05ed8c8f11c2d554690e58734ac204
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-11-24 06:24:42 +0200

    add movement to enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform target;
+    private int currentIndex = 0;
+
+    public float speed = 10f;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(target.position, transform.position) <= 0.3f)
+        {
+            TrackNextWaypoint();
+        }
+    }
+
+    private void TrackNextWaypoint()
+    {
+        if (currentIndex == Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        target = Waypoints.points[++currentIndex];
+    }
+}
