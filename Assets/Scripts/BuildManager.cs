51ae2e98e7bb66075cc82f7fb09d591195c829df
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-11-26 06:50:44 +0200

    add posibility build turrets on map

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    public GameObject standartTurretPref;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Debug.LogError("Another Build Manager instance");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Start()
+    {
+        turretToBuild = standartTurretPref;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private Renderer rendererComponent;
+    private Color initialColor;
+    private GameObject turret;
+
+    private void Start()
+    {
+        rendererComponent = GetComponent<Renderer>();
+        initialColor = rendererComponent.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        rendererComponent.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rendererComponent.material.color = initialColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret)
+        {
+            return;
+        }
+
+        var turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+}
