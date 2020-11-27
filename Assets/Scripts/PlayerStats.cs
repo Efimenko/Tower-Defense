b0335aec9221893eab71f2024d856104b1cf8b51
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-11-27 15:12:48 +0200

    display money count and turrets cost

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public static int money;
     public int initialMoney = 400;
+    public Text moneyText;
 
     private void Start()
     {
         money = initialMoney;
+    }
+
+    private void Update()
+    {
+        moneyText.text = money.ToString();
     }
 }
