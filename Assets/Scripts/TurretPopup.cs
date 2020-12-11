c6029c6daec3462f6dd6cc3444618532d0101f3e
Yefimenko Bohdan <bohdan@seekingalpha.com>
2020-12-11 07:51:08 +0200

    display real upgrade cost in button

---
+++ Assets/Scripts/TurretPopup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurretPopup : MonoBehaviour
+{
+    public Text upgradeCost;
+
+    private void OnEnable()
+    {
+        upgradeCost.text = BuildManager.instance.turretToUpgrade.upgradeCost + "$";
+    }
+}
